5b72aaf8f5643297d40eeeab14c9949c2be7d488
Santiago Herrera Velásquez <santiherreravelas@gmail.com>
2023-10-24 13:22:58 -0500

    orm config

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,7 @@ const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET,
     db_url: process.env.DB_URL,
+    isProd: process.env.NODE_ENV === 'production',
 }
 
 module.exports = { config }
--- src/db/connection.js
+++ src/db/connection.js
@@ -5,19 +5,18 @@ const { config } = require('../config/config')
 
 console.log("config.db_url")
 console.log(config.db_url)
-console.log("postgres://user:password@localhost:5433/movie-review-api")
 
 const options = {
     logging: true
 }
 
-// if (config.isProd) {
-//     options.dialectOptions = {
-//         ssl: {
-//             rejectUnauthorized: false
-//         }
-//     }
-// }
+if (config.isProd) {
+    options.dialectOptions = {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+}
 
 const sequelize = new Sequelize(config.db_url, options);
 
