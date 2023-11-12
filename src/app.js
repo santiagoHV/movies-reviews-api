7fcb1217083d4bcc6dbeeda6162c93a8ca0f6dad
Santiago Herrerea <santiherreravelas@gmail.com>
2023-11-11 21:39:44 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const sequelize = require('./db/connection');
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
